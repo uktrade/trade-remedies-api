6da7925327055b6aa2313ba13fcdb2eab24d047d
Mark Higham <mark.higham@gmail.com>
2020-11-11 18:18:15 +0000

    restrict running bootstrap commands to first instance - this should speed up deployments

---
+++ scripts/entry.sh
@@ -0,0 +1,6 @@
+#!/bin/sh -e
+
+[[ "$INSTANCE_INDEX" == 1 ]] && cd trade_remedies_api && python ./manage.py migrate && python ./manage.py resetsecurity && python ./manage.py adminuser && python ./manage.py loaddata core/fixtures/*.json && python ./manage.py loaddata security/fixtures/*.json && python ./manage.py loaddata organisations/fixtures/*.json && python ./manage.py loaddata cases/fixtures/*.json && python ./manage.py load_sysparams && python ./manage.py s3credentials && python ./manage.py notify_env && python ./manage.py collectstatic --noinput 
+
+cd trade_remedies_api && gunicorn trade_remedies_api.wsgi --bind 0.0.0.0:$API_PORT --config trade_remedies_api/gunicorn.py
+
