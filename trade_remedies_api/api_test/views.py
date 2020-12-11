2a92fdc94a46f77f0f01ca8a5a0ded5bbd6decfd
Ross Miller <github@decodedcollective.com>
2020-12-11 13:53:57 +0000

    Augment BDD infrastructure

---
+++ trade_remedies_api/api_test/views.py
@@ -0,0 +1,8 @@
+from rest_framework.decorators import api_view, permission_classes, authentication_classes
+from rest_framework.response import Response
+
+@api_view()
+@authentication_classes([])
+@permission_classes([])
+def create_test_case(request):
+    return Response({"message": "success"})
