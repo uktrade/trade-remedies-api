911fae0ba712520bf9ad958919289ed132702547
Luisella Strona <luisella21@hotmail.com>
2020-12-16 08:49:18 +0000

    Final version.

--- trade_remedies_api/security/management/commands/patch_user.py
+++ trade_remedies_api/security/management/commands/patch_user.py
@@ -5,26 +5,30 @@ from django.contrib.auth.models import Group
 
 from core.models import User
 
-from security.constants import (
-    SECURITY_GROUP_ORGANISATION_USER,
-)
+from security.constants import SECURITY_GROUP_ORGANISATION_USER
 
 logger = logging.getLogger(__name__)
+
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument(
-            "user_email",
-            help="User's email address",
-            nargs='+',
-            type=str,
+            "user_email", help="User's email address", nargs="+", type=str,
         )
 
+    help = (
+        f"Fix the error on public portal 'Invalid access to environment',  "
+        f"caused by not having a group security assigned. "
+        f"The command gives {SECURITY_GROUP_ORGANISATION_USER} "
+        f"authorization to the user. "
+        f"Pass the user email as parameter."
+    )
+
     def handle(self, *args, **options):
-        logger.info("+ Checking users")
 
         # check that the user exist
-        for user_email in options['user_email']:
+        for user_email in options["user_email"]:
+            logger.info(f"Patching user {user_email}")
             normalised_email = user_email.lower().strip()
             user_obj = User.objects.filter(email__iexact=normalised_email).first()
             if not user_obj:
@@ -34,36 +38,20 @@ class Command(BaseCommand):
 
             # check that the users does not have a group assigned
             if group_queryset.count():
+
                 raise CommandError(f"User with email '{user_email}' has already a group assigned.")
 
             # assign 'Organisation User'
-            # find the group in organisation user
-            org = OrganisationUser.objects.filter(user_id=user_id).first()
-            group_type = org.security_group.name
-            if org:
-                user_obj.groups.add(org.security_group)
+            group_obj = Group.objects.filter(name=SECURITY_GROUP_ORGANISATION_USER).first()
+
+            if group_obj:
+                user_obj.groups.add(group_obj)
                 user_obj.save()
-                self.stdout.write(
-                    self.style.SUCCESS(
-                        f"Successfully set {user_email} as {group_type}."
-                        )
-                    )
+                msg = f"Successfully set {user_email} as {SECURITY_GROUP_ORGANISATION_USER}."
+                logger.info(msg)
+
+                self.stdout.write(self.style.SUCCESS(msg))
             else:
-                pass
-
-        # if a group has been passed as argument, use it
-
-        # otherwise, find the group in organisationuser
-        # check the group assigned to the user in organisationuser
-
-        # if this does not exist, ask to provide a group
-        pass
-
-        # if not password:
-        #     self.stdout.write(
-        #         self.style.ERROR(
-        #             "Please supply a password for this test user"
-        #         )
-        #     )
-        #     return
-
+                raise CommandError(
+                    f"Security group '{SECURITY_GROUP_ORGANISATION_USER}' not defined."
+                )
