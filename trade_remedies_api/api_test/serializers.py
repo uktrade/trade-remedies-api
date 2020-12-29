4e06424fd8750d130eee43da4d42b8d5f2a32b90
Luisella Strona <luisella21@hotmail.com>
2020-12-29 19:03:42 +0000

    Test working.

--- trade_remedies_api/api_test/serializers.py
+++ trade_remedies_api/api_test/serializers.py
@@ -1,18 +1,32 @@
-from rest_framework import serializers, EmailField
+from rest_framework import serializers
 from core.models import User
 
+TEST_PASSWORD= "A7Hhfa!jfaw@f"
+TEST_EMAIL= "ttt.aaa@d.com"
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["id", "..."]  # Add other fields
+        fields = ["id", "email", "group"]
 
 
 class TestUserSerializer(serializers.Serializer):
-    email = EmailField()
+    email = serializers.EmailField(required=False)
+    id = serializers.CharField(required=False)
 
     def create(self, validated_data):
-        user = User.objects.create(**validated_data, username="foo",)
-        user.set_password("password1")
-        user.save()
+        try:
+            email = validated_data.pop("email")
+        except KeyError:
+            email = TEST_EMAIL
+
+        user = User.objects.create_user(
+            name="test user",
+            email=email,
+            password=TEST_PASSWORD,
+            groups=[],
+            country="GB",
+            timezone="Europe/London",
+            phone="012345678",
+        )
         return user
