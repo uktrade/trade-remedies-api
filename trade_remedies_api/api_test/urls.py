e1791aa317779f96512d82e003abbc2e1a0905ca
Luisella Strona <luisella21@hotmail.com>
2020-12-30 17:51:00 +0000

    Tidy up.

--- trade_remedies_api/api_test/urls.py
+++ trade_remedies_api/api_test/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 
 from api_test.views import (
     create_test_case,
-    create_test_user,
     Users,
 )
 
@@ -10,16 +9,6 @@ from trade_remedies_api.urls import urlpatterns
 
 urlpatterns += [
     path("api-test-obj/case/", create_test_case),
-    path(
-        "api-test-obj/create-test-user/<str:email>/<str:password>/<str:group>/",
-        create_test_user,
-    ),
-    path(
-        "api-test-obj/user/",
-        Users.as_view(),
-    ),
-    path(
-        "api-test-obj/user/<str:email>/",
-        Users.as_view(),
-    ),
+    path("api-test-obj/users/", Users.as_view(),),
+    path("api-test-obj/users/<str:email>/", Users.as_view(),),
 ]
