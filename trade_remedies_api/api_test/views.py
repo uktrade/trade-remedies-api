a35c4acfd81c881160ce636a7036f096da86182f
Luisella Strona <luisella21@hotmail.com>
2020-12-31 15:58:06 +0000

    Added clean up of databases after the each scenario.

--- trade_remedies_api/api_test/views.py
+++ trade_remedies_api/api_test/views.py
@@ -1,3 +1,5 @@
+from django.http import Http404
+
 from rest_framework.decorators import api_view, permission_classes, authentication_classes
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -7,28 +9,17 @@ from core.models import User
 
 from api_test.serializers import UserSerializer, TestUserSerializer
 
-
-@api_view()
-@authentication_classes([])
-@permission_classes([])
-def create_test_case(request):
-    return Response({"message": "success"})
+from django.core import management
 
 
 @authentication_classes([])
 @permission_classes([])
-class Users(APIView):
+class UserList(APIView):
     def get(self, request, format=None):
         # Return all users
         users = User.objects.all()
         serializer = UserSerializer(users, many=True)
         return Response(serializer.data)
-
-    # def get(self, request, pk, format=None):
-    #     # Return single user
-    #     user = User.objects.get(pk=pk)
-    #     serializer = UserSerializer(user)
-    #     return Response(serializer.data)
 
     def post(self, request, format=None):
         # Create a test user
@@ -41,3 +32,37 @@ class Users(APIView):
 
         print(f"serializer.errors = {serializer.errors}")
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST,)
+
+
+@authentication_classes([])
+@permission_classes([])
+class UserDetail(APIView):
+    def get_object(self, email):
+        try:
+            return User.objects.get(email=email)
+        except User.DoesNotExist:
+            raise Http404
+
+    def get(self, request, email, format=None):
+        # Return single user
+        user = User.objects.get(email=email)
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+
+    def put(self, request, email, format=None):
+        user = self.get_object(email)
+        serializer = TestUserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED,)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST,)
+
+
+@api_view()
+@authentication_classes([])
+@permission_classes([])
+def restore_database(context):
+    management.call_command("flush", "--noinput")
+    management.call_command("loaddata", "/var/backups/api_test.json")
+    return Response(status.HTTP_200_OK)
