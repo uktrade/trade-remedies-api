bb11e58f6d142e8f5a654d4c95c4d369c5c2e6c9
Luisella Strona <luisella21@hotmail.com>
2021-01-15 14:29:03 +0000

    Added api for organisation

--- trade_remedies_api/api_test/serializers.py
+++ trade_remedies_api/api_test/serializers.py
@@ -1,21 +1,30 @@
 from rest_framework import serializers
 from core.models import User
 
-from security.constants import SECURITY_GROUP_ORGANISATION_USER, SECURITY_GROUP_ORGANISATION_OWNER
+from organisations.models import Organisation
+
+from security.constants import SECURITY_GROUP_ORGANISATION_OWNER
 
 TEST_PASSWORD = "A7Hhfa!jfaw@f"
 TEST_EMAIL = "ttt.aaa@d.com"
 
+class OrganisationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Organisation
+        fields = ["id", "name"]
+
 
 class UserSerializer(serializers.ModelSerializer):
+    organisations=OrganisationSerializer(many=True, read_only=True)
     class Meta:
         model = User
-        fields = ["id", "email"]
+        fields = ["id", "email", "organisations"]
 
 
 class TestUserSerializer(serializers.Serializer):
     email = serializers.EmailField(default=TEST_EMAIL)
     id = serializers.CharField(required=False)
+    organisations=OrganisationSerializer(many=True, read_only=True)
 
     def create(self, validated_data):
         email = validated_data.pop("email")
@@ -33,4 +42,5 @@ class TestUserSerializer(serializers.Serializer):
             organisation_address= 'Test address',
         )
         return user
+
 
