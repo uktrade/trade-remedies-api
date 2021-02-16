ac627be9e944cf1e9588934a5b84c5e1ebee5083
Dave Charles <22290851+davecharles@users.noreply.github.com>
2021-02-16 11:22:49 +0000

    merge: 1.5.7.1 hotfix into develop (trlst 252) (#48)
    Merge 1.5.7.1 hotfix (to make logging more robust) into develop (trlst-252)
    - Django signal receivers used to log db events cannot depend on
    instance attrs being present so made them more forgiving so we
    can guarantee the logging to debug issues. As additional belt
    and braces, updated receivers to not rely on instance presence.
    While this is unlikely, maybe possible if receiver is misconfigured.
    - added pytest catchlog.
    - added tests.
    - added pytest config.

--- trade_remedies_api/cases/receivers.py
+++ trade_remedies_api/cases/receivers.py
@@ -13,35 +13,55 @@ logger = logging.getLogger(__name__)
 
 @receiver(pre_delete, sender=UserCase)
 def log_deleted_usercase(sender, instance, **kwargs):
-    logger.info(
-        f"UserCase record deleted: "
-        f"user_id = {instance.user.id}, "
-        f"email = {instance.user.email}, "
-        f"case_id = {instance.case.id}, "
-        f"case = {instance.case.name}, "
-        f"organisation_id = {instance.organisation.id}, "
-        f"organisation = {instance.organisation.name} "
-    )
+    try:
+        message = (
+            f"UserCase record deleted: "
+            f"user_id = {getattr(instance.user, 'id', 'unknown')}, "
+            f"email = {getattr(instance.user, 'email', 'unknown')}, "
+            f"case_id = {getattr(instance.case, 'id', 'unknown')}, "
+            f"case = {getattr(instance.case, 'name', 'unknown')}, "
+            f"organisation_id = {getattr(instance.organisation, 'id', 'unknown')}, "
+            f"organisation = {getattr(instance.organisation, 'name', 'unknown')} "
+        )
+    except AttributeError as e:
+        message = f"UserCase record deleted: Unable to log all details because: {e}"
+    logger.info(message)
 
 
 @receiver(pre_delete, sender=User)
 def log_deleted_user(sender, instance, **kwargs):
-    logger.info(f"User record deleted: "
-                f"user_id = {instance.id}, "
-                f"email = {instance.email}, ")
+    try:
+        message = (
+            f"User record deleted: "
+            f"user_id = {getattr(instance, 'id', 'unknown')}, "
+            f"email = {getattr(instance, 'email', 'unknown')}"
+        )
+    except AttributeError as e:
+        message = f"User record deleted: Unable to log all details because: {e}"
+    logger.info(message)
 
 
 @receiver(pre_delete, sender=Case)
 def log_deleted_case(sender, instance, **kwargs):
-    logger.info(f"Case record deleted: "
-                f"case_id = {instance.id},"
-                f" case = {instance.name}, ")
+    try:
+        message = (
+            f"Case record deleted: "
+            f"case_id = {getattr(instance, 'id', 'unknown')}, "
+            f"case = {getattr(instance, 'name', 'unknown')}"
+        )
+    except AttributeError as e:
+        message = f"Case record deleted: Unable to log all details because: {e}"
+    logger.info(message)
 
 
 @receiver(pre_delete, sender=Organisation)
 def log_deleted_organisation(sender, instance, **kwargs):
-    logger.info(
-        f"Organisation record deleted: "
-        f"organisation_id = {instance.id}, "
-        f"organisation = {instance.name}, "
-    )
+    try:
+        message = (
+            f"Organisation record deleted: "
+            f"organisation_id = {getattr(instance, 'id', 'unknown')}, "
+            f"organisation = {getattr(instance, 'name', 'unknown')}"
+        )
+    except AttributeError as e:
+        message = f"Organisation record deleted: Unable to log all details because: {e}"
+    logger.info(message)
