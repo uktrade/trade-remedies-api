1b12a44674e496f735d2cdb54cf156497ec4d9a7
Luisella Strona <36708790+Luisella21@users.noreply.github.com>
2021-02-22 10:56:39 +0000

    feat: trlst-40 document submission sub-categories
    As a caseworker, I need the ability to ADD a subcategory when I publish a document so that it is clearer for the users of the Public site to see what the publication is for, and specifically fulfil one of the important USFTA requirements. 
    
    - Removed json fixture files in favour of adding initial data for  submission type and submission status values.
    - Added migrations to add data.
    - Added non-automatic key fields to Submission and SubmissionType models.

--- update_fixtures.sh
+++ update_fixtures.sh
@@ -12,10 +12,6 @@ echo "Case types"
 ./manage.py dumpdata cases.casetype --indent 4 > cases/fixtures/case_types.json
 echo "Company sectors"
 ./manage.py dumpdata cases.sector --indent 4 > cases/fixtures/sectors.json
-echo "Submission statuses"
-./manage.py dumpdata cases.submissionstatus --indent 4 > cases/fixtures/submission_status.json
-echo "Submission types"
-./manage.py dumpdata cases.submissiontype --indent 4 > cases/fixtures/submission_types.json
 echo "Archive reasons"
 ./manage.py dumpdata cases.archivereason --indent 4 > cases/fixtures/archive_reasons.json
 
