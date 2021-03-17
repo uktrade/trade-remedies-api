feefdba8c39a986328c1e443b30c9777a335ea5d
davecharles <22290851+davecharles@users.noreply.github.com>
2021-03-17 09:48:14 +0000

    fix: add extra logging, exception handing and todo's

--- trade_remedies_api/documents/av_scan.py
+++ trade_remedies_api/documents/av_scan.py
@@ -10,25 +10,35 @@ from requests_toolbelt.multipart.encoder import MultipartEncoder
 from .utils import s3_client
 from .models import Document
 
-
 logger = getLogger(__name__)
 
 
 class S3StreamingBodyWrapper:
     """S3 Object wrapper that plays nice with streamed multipart/form-data."""
 
-    def __init__(self, s3_obj):
+    def __init__(self, s3_obj, name):
         """Init wrapper, and grab interesting bits from s3 object."""
         self._obj = s3_obj
         self._body = s3_obj["Body"]
-        self._remaining_bytes = s3_obj["ContentLength"]
+        self.total_length = self._remaining_bytes = s3_obj["ContentLength"]
+        self.name = name
+        self.logged = set()
+        logger.info(f"Streaming {self.name}: {self.total_length} bytes")
 
     def read(self, amt=-1):
         """Read given amount of bytes, and decrease remaining len."""
         content = self._body.read(amt)
         self._remaining_bytes -= len(content)
-
+        self.log_progress()
         return content
+
+    def log_progress(self):
+        completed_pct = int(100 - (self._remaining_bytes/self.total_length * 100))
+        if completed_pct % 10:
+            return
+        if completed_pct not in self.logged:
+            self.logged.add(completed_pct)
+            logger.info(f"{self.name}: AV check {completed_pct}% complete")
 
     def __len__(self):
         """Return remaining bytes, that have not been read yet.
@@ -38,6 +48,7 @@ class S3StreamingBodyWrapper:
         return self._remaining_bytes
 
 
+# TODO - Remove cruft
 def get_s3_client():
     # DEPRECATED
     s3 = boto3.client("s3")
@@ -53,8 +64,10 @@ def virus_scan_document(document_pk: str):
     try:
         with advisory_lock(f"av-scan-{document_pk}"):
             _process_document(document_pk)
-    except Exception:
-        logger.exception("Error scanning document for viruses")
+    except VirusScanException as e:
+        logger.critical(f"{e}")
+    except Document.DoesNotExist:
+        logger.error(f"Cannot AV scan nonexistent document with id: {document_pk}")
 
 
 def _process_document(document_pk: str):
@@ -65,22 +78,21 @@ def _process_document(document_pk: str):
         )
     doc = Document.objects.get(pk=document_pk)
     if doc.virus_scanned_at is not None and doc.safe is not None:
-        warn_msg = (
-            f"Skipping scan of doc:{document_pk}, already performed on {doc.virus_scanned_at}"
+        logger.info(
+            f"Skipping scan of doc:{document_pk}, already performed " f"on {doc.virus_scanned_at}"
         )
-        logger.info(warn_msg)
         return
     try:
-        is_file_clean = _scan_s3_object(doc.file.name, doc.s3_bucket, doc.s3_key)
+        is_file_clean = _scan_s3_object(doc.name, doc.s3_bucket, doc.s3_key)
         if is_file_clean is not None:
             doc.virus_scanned_at = now()
             doc.safe = is_file_clean
             doc.save()
-    except Exception:
+    except Exception as e:
+        logger.critical(f"Failed to AV scan document: {e}")
         doc.safe = None
         doc.virus_scanned_at = None
         doc.save()
-        return
 
 
 def _scan_s3_object(original_filename, bucket, key):
@@ -89,13 +101,21 @@ def _scan_s3_object(original_filename, bucket, key):
     response = _client.get_object(Bucket=bucket, Key=key)
     with closing(response["Body"]):
         return _scan_raw_file(
-            original_filename, S3StreamingBodyWrapper(response), response["ContentType"]
+            original_filename,
+            S3StreamingBodyWrapper(response, original_filename),
+            response["ContentType"],
         )
 
 
 def _scan_raw_file(filename, file_object, content_type):
     """Virus scans a file-like object."""
-    multipart_fields = {"file": (filename, file_object, content_type,)}
+    multipart_fields = {
+        "file": (
+            filename,
+            file_object,
+            content_type,
+        )
+    }
     encoder = MultipartEncoder(fields=multipart_fields)
 
     response = requests.post(
@@ -113,5 +133,6 @@ def _scan_raw_file(filename, file_object, content_type):
     return not report.get("malware")
 
 
+# TODO - Move to top
 class VirusScanException(Exception):
     """Exceptions raised when scanning documents for viruses."""
