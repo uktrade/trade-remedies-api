90db1df831aa53f6d4df1ffe3f805ffd2b8a2f27
nboyse <natasha.boyse@digital.trade.gov.uk>
2021-10-01 10:20:20 +0100

    dave comments

--- trade_remedies_api/audit/admin.py
+++ trade_remedies_api/audit/admin.py
@@ -6,10 +6,6 @@ class AuditAdmin(admin.ModelAdmin):
     search_fields = ["type", "case_id", "content_type__model", "milestone", "created_at"]
     list_display = ("type", "created_at", "case_id", "content_type", "milestone")
     list_filter = ["type", "case_id"]
-    # Leave commented out until all environments have run migrations
-    # for removal of audit.case foreign key.
-    # list_display = ('type', 'created_at', 'case_id', 'content_type', 'milestone')
-    # list_filter = ['type', 'case_id']
 
 
 admin.site.register(Audit, AuditAdmin)
