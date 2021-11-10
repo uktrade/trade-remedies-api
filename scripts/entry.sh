854e548f8cd73110880f7acfa56264111ffa79e0
davecharles <22290851+davecharles@users.noreply.github.com>
2021-11-10 15:53:25 +0000

    fix: update PaaS config

--- scripts/entry.sh
+++ scripts/entry.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -e
 
 cd ${HOME}/trade_remedies_api
-python ./manage.py migrate 
+python ./manage.py migrate
 python ./manage.py load_sysparams
 python ./manage.py notify_env
-python ./manage.py collectstatic --noinput 
+python ./manage.py collectstatic --noinput
 
-gunicorn trade_remedies_api.wsgi --bind 0.0.0.0:$API_PORT --capture-output --config trade_remedies_api/gunicorn.py
+gunicorn config.wsgi --bind 0.0.0.0:$API_PORT --capture-output --config config/gunicorn.py
