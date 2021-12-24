21fcd4c02bbb7382e7299af5737ab24941ccca03
davecharles <22290851+davecharles@users.noreply.github.com>
2021-12-24 16:27:16 +0000

    fix: update app test.

--- trade_remedies_api/tests/authentication/test_app.py
+++ trade_remedies_api/tests/authentication/test_app.py
@@ -5,4 +5,4 @@ def test_app_config():
     app = django_apps.get_app_config("authentication")
     assert app.name == "authentication"
     assert app.label == "authentication"
-    assert app.verbose_name == "Authentication Application"
+    assert app.verbose_name == "Authentication V2"
