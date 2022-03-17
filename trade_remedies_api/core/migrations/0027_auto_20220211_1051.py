61a7573d2e95db5b2e9ceae42d937df11471eb6a
Christopher Pettinga <chrispettinga@gmail.com>
2022-03-17 09:18:54 +0000

    Resolved migration conflicts

--- trade_remedies_api/core/migrations/0027_auto_20220211_1051.py
+++ trade_remedies_api/core/migrations/0027_auto_20220211_1051.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0025_auto_20220211_1003'),
+        ('core', '0026_auto_20220211_1003'),
     ]
 
     operations = [
