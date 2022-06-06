86289e4f538ee0360f81859b986d31bdfd2f74b0
Johnny <knowyourlines@outlook.com>
2022-06-06 15:53:19 +0100

    test authenticated requests

--- trade_remedies_api/functional_tests/test_password_reset.py
+++ trade_remedies_api/functional_tests/test_password_reset.py
@@ -4,16 +4,16 @@ from rest_framework import status
 
 class PasswordResetTest(APITransactionTestCase):
     def test_requests_password_reset(self):
-        # self.client.force_authenticate(user=self.user_1, token=self.user_1.auth_token)
+        self.client.force_authenticate()
         response = self.client.get("/api/v2/accounts/password/request_reset/")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_requests_password_reset_via_request_id(self):
-        # self.client.force_authenticate(user=self.user_1, token=self.user_1.auth_token)
+        self.client.force_authenticate()
         response = self.client.get("/api/v2/accounts/password/request_reset/")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_resets_password(self):
-        # self.client.force_authenticate(user=self.user_1, token=self.user_1.auth_token)
+        self.client.force_authenticate()
         response = self.client.get("/api/v2/accounts/password/reset_form/")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
