d90ee50ffb735c44e89f1d7270e39d5952d147c1
Johnny <knowyourlines@outlook.com>
2022-06-06 17:05:50 +0100

    print statement

--- trade_remedies_api/functional_tests/test_password_reset.py
+++ trade_remedies_api/functional_tests/test_password_reset.py
@@ -6,14 +6,17 @@ class PasswordResetTest(APITransactionTestCase):
     def test_requests_password_reset(self):
         self.client.force_authenticate()
         response = self.client.get("/api/v2/accounts/password/request_reset/")
+        print(response.data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_requests_password_reset_via_request_id(self):
         self.client.force_authenticate()
         response = self.client.get("/api/v2/accounts/password/request_reset/")
+        print(response.data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_resets_password(self):
         self.client.force_authenticate()
         response = self.client.get("/api/v2/accounts/password/reset_form/")
+        print(response.data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
