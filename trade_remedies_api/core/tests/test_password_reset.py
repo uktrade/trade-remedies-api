9b005262b05d2d4261f7310d1f491f707e6a8aea
Johnny <knowyourlines@outlook.com>
2022-06-06 21:07:55 +0100

    update test

--- trade_remedies_api/core/tests/test_password_reset.py
+++ trade_remedies_api/core/tests/test_password_reset.py
@@ -15,14 +15,21 @@ class PasswordResetTest(TestCase):
 
     def setUp(self):
         Group.objects.create(name="Organisation Owner")
-        self.user = User.objects.create(email="harel@harelmalka.com", name="Joe Public")  # /PS-IGNORE
+        self.user = User.objects.create(
+            email="harel@harelmalka.com", name="Joe Public"  # /PS-IGNORE
+        )
         self.user.set_password(PASSWORD)
         self.user.save()
 
     def test_reset_request(self):
-        reset, send_report = PasswordResetRequest.objects.reset_request(self.user.email)
+        reset, send_report, reset_token = PasswordResetRequest.objects.reset_request(
+            self.user.email
+        )
+        assert isinstance(reset_token, str)
         assert type(reset) is PasswordResetRequest
-        assert bool(reset.token)  # Checking that the method actually generates and attaches a token to the object
+        assert bool(
+            reset.token
+        )  # Checking that the method actually generates and attaches a token to the object
 
     def test_reset_is_valid(self):
         reset_valid = PasswordResetRequest.objects.create(user=self.user)
@@ -30,9 +37,7 @@ class PasswordResetTest(TestCase):
 
         self.assertTrue(
             PasswordResetRequest.objects.validate_token(
-                token=token,
-                user_pk=self.user.pk,
-                validate_only=True
+                token=token, user_pk=self.user.pk, validate_only=True
             )
         )
 
@@ -42,9 +47,7 @@ class PasswordResetTest(TestCase):
 
         self.assertFalse(
             PasswordResetRequest.objects.validate_token(
-                token='123das',
-                user_pk=self.user.pk,
-                validate_only=True
+                token="123das", user_pk=self.user.pk, validate_only=True
             )
         )
 
@@ -63,7 +66,7 @@ class PasswordResetTest(TestCase):
     def test_password_reset_works(self):
         reset = PasswordResetRequest.objects.create(user=self.user)
         token = reset.generate_token()
-        new_password = 'New!Passw0rd'
+        new_password = "New!Passw0rd"
 
         # First we check if the password_reset method actually changes the password and returns the User object
         reset_user = PasswordResetRequest.objects.password_reset(token, self.user.pk, new_password)
@@ -71,7 +74,7 @@ class PasswordResetTest(TestCase):
         self.user.refresh_from_db()
         self.assertTrue(self.user.check_password(new_password))
 
-        new_password = 'New!PASDD123!'
+        new_password = "New!PASDD123!"
         # Now we try the password reset again, as the token is invalid it should return None and not change the password
         reset_user = PasswordResetRequest.objects.password_reset(token, self.user.pk, new_password)
         self.assertEqual(reset_user, None)
