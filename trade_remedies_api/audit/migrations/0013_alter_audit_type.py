bc60be27cd6d638d3de05f678c39ac54d41c28d0
Johnny <knowyourlines@outlook.com>
2022-06-06 20:57:37 +0100

    precommit hooks

--- trade_remedies_api/audit/migrations/0013_alter_audit_type.py
+++ trade_remedies_api/audit/migrations/0013_alter_audit_type.py
@@ -6,13 +6,32 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('audit', '0012_alter_audit_type'),
+        ("audit", "0012_alter_audit_type"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='audit',
-            name='type',
-            field=models.CharField(choices=[('UPDATE', 'Update'), ('CREATE', 'Create'), ('DELETE', 'Delete'), ('PURGE', 'Purge'), ('RESTORE', 'Restore'), ('READ', 'Read'), ('LOGIN', 'Log In'), ('LOGIN_FAILED', 'Log In Failed'), ('LOGOUT', 'Log Out'), ('EVENT', 'Event'), ('ATTACH', 'Attach'), ('NOTIFY', 'Notify'), ('DELIVERED', 'Delivery'), ('PASSWORD_RESET', 'Password Reset'), ('PASSWORD_RESET_FAILED', 'Password Reset Failed')], max_length=50),
+            model_name="audit",
+            name="type",
+            field=models.CharField(
+                choices=[
+                    ("UPDATE", "Update"),
+                    ("CREATE", "Create"),
+                    ("DELETE", "Delete"),
+                    ("PURGE", "Purge"),
+                    ("RESTORE", "Restore"),
+                    ("READ", "Read"),
+                    ("LOGIN", "Log In"),
+                    ("LOGIN_FAILED", "Log In Failed"),  # /PS-IGNORE
+                    ("LOGOUT", "Log Out"),  # /PS-IGNORE
+                    ("EVENT", "Event"),
+                    ("ATTACH", "Attach"),
+                    ("NOTIFY", "Notify"),
+                    ("DELIVERED", "Delivery"),
+                    ("PASSWORD_RESET", "Password Reset"),  # /PS-IGNORE
+                    ("PASSWORD_RESET_FAILED", "Password Reset Failed"),  # /PS-IGNORE
+                ],
+                max_length=50,
+            ),
         ),
     ]
