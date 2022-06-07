f168499c12cedb245d1dedb23ea6576fcdd4f7b4
Nawaz <nawaz.ahmed@digital.trade.gov.uk>
2022-06-07 15:10:40 +0100

    black made changes

--- trade_remedies_api/security/management/commands/new_case_role.py
+++ trade_remedies_api/security/management/commands/new_case_role.py
@@ -13,31 +13,37 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         # Positional aruguments - order in which they need to be entered
         # Note: using nargs paramater causes the data to be passed in a list
-        parser.add_argument('name', type=str, help='CaseRole name. E.g., "Overseas Producer"')
-        parser.add_argument('plural', type=str, help='CaseRole plural. E.g., "Overseas Producers"')
-        parser.add_argument('key', type=str, help='CaseRole key. E.g., overseas_producer')
-        parser.add_argument('order', type=int, help='CaseRole order. E.g., 45')
+        parser.add_argument(
+            "name", type=str, help='CaseRole name. E.g., "Overseas Producer"'
+        )
+        parser.add_argument(
+            "plural", type=str, help='CaseRole plural. E.g., "Overseas Producers"'
+        )
+        parser.add_argument(
+            "key", type=str, help="CaseRole key. E.g., overseas_producer"
+        )
+        parser.add_argument("order", type=int, help="CaseRole order. E.g., 45")
 
         # Optional aruguments
-        parser.add_argument('--factor', type=int, help='To multiple "order" value by')
+        parser.add_argument("--factor", type=int, help='To multiple "order" value by')
 
     def handle(self, *args, **options):
 
-        if options['factor']:
-            # Increase gap between existing "order" values to make (possible) future 
+        if options["factor"]:
+            # Increase gap between existing "order" values to make (possible) future
             # insertions easier.
             logger.info(f"Amending order values.")
             for case_role in CaseRole.objects.all():
-                case_role.order *= options['factor']
+                case_role.order *= options["factor"]
                 case_role.save()
 
         logger.info(f"Creating new case role: {options['name']}")
         new_case_role = CaseRole.objects.create(
-            name=options['name'],
-            plural=options['plural'],
-            key=options['key'],
-            order=options['order']
-            )
+            name=options["name"],
+            plural=options["plural"],
+            key=options["key"],
+            order=options["order"],
+        )
 
         # get case actions and add to case role
         view_application = CaseAction.objects.get(id="VIEW_APPLICATION")
