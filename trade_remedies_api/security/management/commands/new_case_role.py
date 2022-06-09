20979a6c0fc225dd2d617739afaf41dafe58d5d4
Nawaz <nawaz.ahmed@digital.trade.gov.uk>
2022-06-09 09:55:50 +0100

    add new case role management command tests

--- trade_remedies_api/security/management/commands/new_case_role.py
+++ trade_remedies_api/security/management/commands/new_case_role.py
@@ -13,15 +13,9 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         # Positional aruguments - order in which they need to be entered
         # Note: using nargs paramater causes the data to be passed in a list
-        parser.add_argument(
-            "name", type=str, help='CaseRole name. E.g., "Overseas Producer"'
-        )
-        parser.add_argument(
-            "plural", type=str, help='CaseRole plural. E.g., "Overseas Producers"'
-        )
-        parser.add_argument(
-            "key", type=str, help="CaseRole key. E.g., overseas_producer"
-        )
+        parser.add_argument("name", type=str, help='CaseRole name. E.g., "Overseas Producer"')
+        parser.add_argument("plural", type=str, help='CaseRole plural. E.g., "Overseas Producers"')
+        parser.add_argument("key", type=str, help="CaseRole key. E.g., overseas_producer")
         parser.add_argument("order", type=int, help="CaseRole order. E.g., 45")
 
         # Optional aruguments
@@ -32,10 +26,12 @@ class Command(BaseCommand):
         if options["factor"]:
             # Increase gap between existing "order" values to make (possible) future
             # insertions easier.
-            logger.info("Amending order values.")
+            logger.info("Amending order values")
             for case_role in CaseRole.objects.all():
                 case_role.order *= options["factor"]
                 case_role.save()
+
+            self.stdout.write(self.style.SUCCESS(f"Order values multiplied by {options['factor']}"))
 
         logger.info(f"Creating new case role: {options['name']}")
         new_case_role = CaseRole.objects.create(
@@ -49,3 +45,5 @@ class Command(BaseCommand):
         view_application = CaseAction.objects.get(id="VIEW_APPLICATION")
         view_case = CaseAction.objects.get(id="VIEW_CASE")
         new_case_role.actions.add(view_application, view_case)
+
+        self.stdout.write(self.style.SUCCESS(f"New CaseRole created: {options['name']}"))
