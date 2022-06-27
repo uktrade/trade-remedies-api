0aee8b04a7b7453ef0479cb092843a9e1b2026a6
jonathanli <jonathan.li@digital.trade.gov.uk>
2022-06-27 13:31:03 +0100

    add new error for no org type chosen

--- trade_remedies_api/core/validation_errors.py
+++ trade_remedies_api/core/validation_errors.py
@@ -71,6 +71,11 @@ validation_errors = {
         "error_text": "Enter your name",
         "error_summary": "You need to enter your name",
     },
+    "no_org_type_chosen": {
+        "field": "org",
+        "error_text": "Select who is registering interest",
+        "error_summary": "You need to select who is registering interest in this case",
+    },
     "no_email_entered": {
         "field": "email",
         "error_text": "Enter your email address",
