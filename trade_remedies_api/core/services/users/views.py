0ec23f8557dfb2a539d6fac351de9d109d33b9cb
christopherpettinga <chrispettinga@gmail.com>
2022-07-04 13:25:14 +0100

    Added tests for the UserViewSet

--- trade_remedies_api/core/services/users/views.py
+++ trade_remedies_api/core/services/users/views.py
@@ -10,16 +10,16 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
-    @action(detail=True, methods=['put'])
+    @action(detail=True, methods=['put'], url_name="change_group")
     def add_group(self, request, *args, **kwargs):
-        group_object = Group.objects.get(name=request.POST["group_name"])
+        group_object = Group.objects.get(name=request.data["group_name"])
         user_object = User.objects.get(pk=kwargs["pk"])
         user_object.groups.add(group_object)
         return self.retrieve(request, *args, **kwargs)
 
     @add_group.mapping.delete
     def delete_group(self, request, *args, **kwargs):
-        group_object = Group.objects.get(name=request.POST["group_name"])
+        group_object = Group.objects.get(name=request.data["group_name"])
         user_object = User.objects.get(pk=kwargs["pk"])
         user_object.groups.remove(group_object)
         return self.retrieve(request, *args, **kwargs)
