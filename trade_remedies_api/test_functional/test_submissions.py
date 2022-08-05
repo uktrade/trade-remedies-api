9416ec2ade7b3ad1be45983691619e27f205dd16
christopherpettinga <chrispettinga@gmail.com>
2022-08-05 16:18:49 +0100

    Creating tests for the new API endpoints

---
+++ trade_remedies_api/test_functional/test_submissions.py
@@ -0,0 +1,37 @@
+from rest_framework.reverse import reverse
+from rest_framework.test import APITransactionTestCase
+
+from rest_framework.test import APITransactionTestCase
+
+from cases.constants import SUBMISSION_TYPE_REGISTER_INTEREST
+from cases.models import Case, CaseType, Submission, SubmissionStatus, SubmissionType
+from config.test_bases import UserSetupTestBase
+from test_functional import FunctionalTestBase
+
+
+class TestSubmissionAPI(FunctionalTestBase):
+    def setUp(self) -> None:
+        super().setUp()
+        self.case_object = Case.objects.create(
+            name="test case",
+            type=CaseType.objects.get(acronym="AD")
+        )
+        roi_submission_type = SubmissionType.objects.get(id=SUBMISSION_TYPE_REGISTER_INTEREST)
+
+        self.submission_object = Submission.objects.create(
+            case=self.case_object,
+            type=roi_submission_type,
+            created_by=self.user,
+            status=roi_submission_type.default_status
+        )
+
+
+    def test_update_submission_status(self):
+        response = self.client.put(
+            f"/api/v2/submissions/{self.submission_object.pk}/update_submission_status/",
+            data={
+                "new_status": "received"
+            }
+        )
+        print('asd')
+
