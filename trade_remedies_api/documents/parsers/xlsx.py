e0e33a48fcaeb6d07a991bdb26f5dbefd9fdc510
Christopher Pettinga <chrispettinga@gmail.com>
2022-09-15 14:43:02 +0100

    Rebranched from develop (#264)

--- trade_remedies_api/documents/parsers/xlsx.py
+++ trade_remedies_api/documents/parsers/xlsx.py
@@ -1,6 +1,7 @@
 from io import BytesIO
 
 import openpyxl
+from openpyxl.chartsheet import Chartsheet
 
 
 def parse(document):
@@ -9,6 +10,11 @@ def parse(document):
     document.file.close()
     for sheet in workbook.sheetnames:
         ws = workbook[sheet]
+        if isinstance(ws, Chartsheet):
+            # ChartSheets are sheets in Excel workbooks containing only a graph and nothing else,
+            # they cause bugs when we attempt to parse them and so let's ignore them as their data
+            # is not searchable anyway.
+            continue
         col_range = ws[ws.min_column : ws.max_column]
         header_col_range = col_range[0]
         for cell in header_col_range:
