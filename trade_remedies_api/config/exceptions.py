eca373293a532ce5ac2a2c9f778b70cfe611ee01
christopherpettinga <chrispettinga@gmail.com>
2022-09-22 17:26:29 +0100

    Added better error handling for Serializer errors, these are now nicely handled by the client and displayed (if debug is False) to the developer.
    For the end-user, this also stops those annoying 20 second timeouts before errors appearing.

---
+++ trade_remedies_api/config/exceptions.py
@@ -0,0 +1,12 @@
+from rest_framework.exceptions import APIException
+
+
+class InvalidSerializerError(APIException):
+    """Trying to save a serializer with invalid data, naughty!"""
+    status_code = 400
+    default_code = "save_serializer_invalid_error"
+
+    def __init__(self, serializer, detail=None, code=None):
+        super().__init__(detail, code)
+        self.detail["exception_type"] = self.default_code
+        self.detail["serializer_name"] = serializer.__class__.__name__
