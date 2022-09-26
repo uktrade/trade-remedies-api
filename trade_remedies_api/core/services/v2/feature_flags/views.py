b1239cc5f5c2c5e80ce4be5a14069abc2b68a8c5
Christopher Pettinga <chrispettinga@gmail.com>
2022-09-26 11:49:49 +0100

    Fix - feature flags - Master (#273)
    * Refactored feature flags viewset and serializer to be more efficient. And moved URL registration to main urls.py
    
    * black formatting and fixing broken feature flag tests

--- trade_remedies_api/core/services/v2/feature_flags/views.py
+++ trade_remedies_api/core/services/v2/feature_flags/views.py
@@ -1,18 +1,14 @@
 from flags.sources import get_flags
-from rest_framework import viewsets
+from rest_framework import exceptions, viewsets
 from rest_framework.response import Response
 
-from config.renderers import APIResponseRenderer
 from core.services.v2.feature_flags.serializers import FlagSerializer
-from rest_framework import exceptions
 
 
 class FlagViewSet(viewsets.ViewSet):
     """
     GenericViewSet to retrieve feature flags in the codebase.
     """
-
-    renderer_classes = (APIResponseRenderer,)
 
     def dispatch(self, request, *args, **kwargs):
         self.flags = get_flags()
