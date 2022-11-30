7ce3e39e972ea02d88f953cad40c1a98dca467a9
christopherpettinga <chrispettinga@gmail.com>
2022-11-30 14:11:21 +0000

    Added tests for feedbackserializer

--- trade_remedies_api/core/services/v2/feedback/tests/test_serializers.py
+++ trade_remedies_api/core/services/v2/feedback/tests/test_serializers.py
@@ -1,12 +1,26 @@
 from config.test_bases import CaseSetupTestMixin
-from core.services.v2.users.serializers import ContactSerializer, UserSerializer
+from core.models import Feedback
+from core.services.v2.feedback.serializers import FeedbackSerializer
 
 
-
-class TestContactSerializer(CaseSetupTestMixin):
-    def test_lowercase_email(self):
-        serializer = ContactSerializer(
-            data={"name": "test", "email": "mixEDEmail@example.cOM"}  # /PS-IGNORE
+class TestFeedbackSerializer(CaseSetupTestMixin):
+    @classmethod
+    def setUpClass(cls):
+        super().setUp()
+        cls.feedback_object = Feedback.objects.create(
+            rating=2,
+            what_didnt_work_so_well=["not_enough_guidance", "other_issue"],
+            logged_in=False,
+            url="/",
         )
-        new_contact = serializer.save()
-        assert new_contact.email.islower()
+
+    def test_verbose_rating_name(self):
+        serializer = FeedbackSerializer(instance=self.feedback_object)
+        assert serializer.data["verbose_rating_name"] == "Dissatisfied"
+
+    def test_verbose_what_didnt_go_so_well(self):
+        serializer = FeedbackSerializer(instance=self.feedback_object)
+        assert serializer.data["verbose_what_didnt_go_so_well"] == [
+            "Not enough guidance",
+            "Other issue",
+        ]
