e4ca7dbaba266393954b164841502db126789a2e
christopherpettinga <chrispettinga@gmail.com>
2022-12-15 10:10:50 +0000

    imports

--- trade_remedies_api/organisations/services/v2/views.py
+++ trade_remedies_api/organisations/services/v2/views.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.models import Group
 from django.contrib.postgres.search import TrigramSimilarity
-from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
