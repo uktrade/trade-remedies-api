cfa22cf1276003809052801e1e34f2f877593155
christopherpettinga <chrispettinga@gmail.com>
2023-02-10 14:55:27 +0000

    Refactored the status field on Invitation serializer and added some tests

--- trade_remedies_api/invitations/services/v2/tests/test_serializers.py
+++ trade_remedies_api/invitations/services/v2/tests/test_serializers.py
@@ -1,11 +1,15 @@
-from config.test_bases import OrganisationSetupTestMixin
+from django.utils import timezone
+
+from cases.constants import SUBMISSION_DOCUMENT_TYPE_CUSTOMER, SUBMISSION_TYPE_INVITE_3RD_PARTY
+from cases.models import Submission, SubmissionDocumentType, get_submission_type
+from config.test_bases import CaseSetupTestMixin
 from invitations.models import Invitation
 from invitations.services.v2.serializers import InvitationSerializer
 from security.constants import SECURITY_GROUP_ORGANISATION_USER
 from security.models import CaseRole
 
 
-class TestInvitationSerializer(OrganisationSetupTestMixin):
+class TestInvitationSerializer(CaseSetupTestMixin):
     def setUp(self) -> None:
         super().setUp()
         self.invitation_object = Invitation.objects.create(
@@ -32,3 +36,81 @@ class TestInvitationSerializer(OrganisationSetupTestMixin):
         )
         assert serializer.is_valid()
         assert serializer.validated_data["case_role"] == applicant_case_role
+
+    def test_draft_status(self):
+        serializer = InvitationSerializer(instance=self.invitation_object)
+        assert serializer.is_valid()
+        assert serializer.validated_data["status"] == ("draft", "Draft")
+
+    def test_accepted_status(self):
+        self.invitation_object.invitation_type = 1
+        self.invitation_object.accepted_at = timezone.now()
+        self.invitation_object.email_sent = True
+        self.invitation_object.save()
+
+        serializer = InvitationSerializer(instance=self.invitation_object)
+        assert serializer.is_valid()
+        assert serializer.validated_data["status"] == ("accepted", "Accepted")
+
+    def test_invite_sent_status(self):
+        self.invitation_object.invitation_type = 1
+        self.invitation_object.email_sent = True
+        self.invitation_object.save()
+
+        serializer = InvitationSerializer(instance=self.invitation_object)
+        assert serializer.is_valid()
+        assert serializer.validated_data["status"] == ("invite_sent", "Invite sent")
+
+    def test_invite_waiting_tra_review_status(self):
+        self.invitation_object.invitation_type = 2
+        self.invitation_object.email_sent = True
+        self.invitation_object.save()
+
+        serializer = InvitationSerializer(instance=self.invitation_object)
+        assert serializer.is_valid()
+        assert serializer.validated_data["status"] == ("waiting_tra_review", "Waiting TRA Approval")
+
+    def test_invite_rejected_by_tra_status(self):
+        self.invitation_object.invitation_type = 2
+        self.invitation_object.email_sent = True
+        self.invitation_object.rejected_at = timezone.now()
+        self.invitation_object.save()
+
+        serializer = InvitationSerializer(instance=self.invitation_object)
+        assert serializer.is_valid()
+        assert serializer.validated_data["status"] == ("rejected_by_tra", "Rejected by the TRA")
+
+    def test_invite_approved_by_tra_status(self):
+        self.invitation_object.invitation_type = 2
+        self.invitation_object.email_sent = True
+        self.invitation_object.approved_at = timezone.now()
+        self.invitation_object.save()
+
+        serializer = InvitationSerializer(instance=self.invitation_object)
+        assert serializer.is_valid()
+        assert serializer.validated_data["status"] == ("approved_by_tra", "Approved by the TRA")
+
+    def test_invite_submission_deficient_status(self):
+        SubmissionDocumentType.objects.create(
+            id=SUBMISSION_DOCUMENT_TYPE_CUSTOMER, key="respondent", name="Customer Document"
+        )
+
+        submission_type = get_submission_type(SUBMISSION_TYPE_INVITE_3RD_PARTY)
+        submission_status = submission_type.deficient_status
+        submission = Submission.objects.create(
+            name="Invite 3rd party",
+            type=submission_type,
+            status=submission_status,
+            case=self.case_object,
+            contact=self.contact_object,
+            organisation=self.organisation,
+        )
+        self.invitation_object.invitation_type = 2
+        self.invitation_object.submission = submission
+        self.invitation_object.email_sent = True
+        self.invitation_object.approved_at = timezone.now()
+        self.invitation_object.save()
+
+        serializer = InvitationSerializer(instance=self.invitation_object)
+        assert serializer.is_valid()
+        assert serializer.validated_data["status"] == ("deficient", "Deficient")
