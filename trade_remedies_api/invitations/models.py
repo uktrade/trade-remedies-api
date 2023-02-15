f50b49d6aeef98262761dd5db3561fc9d205915f
Nawaz <nawaz.ahmed@digital.trade.gov.uk>
2023-02-15 09:49:57 +0000

    add organisation_details_captured to invitation model

--- trade_remedies_api/invitations/models.py
+++ trade_remedies_api/invitations/models.py
@@ -326,6 +326,8 @@ class Invitation(BaseModel):
 
     objects = InvitationManager()
 
+    organisation_details_captured = models.BooleanField(null=True)
+
     def __str__(self):
         return f"{self.organisation} invites {self.contact}"
 
