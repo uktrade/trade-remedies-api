cd825fb7f9bdb65f743b04fc16a2f86f3485eece
Uka Osim <hsojo91@gmail.com>
2023-02-27 12:55:41 +0000

    fixed broken redis health check

--- trade_remedies_api/core/tests/test_service_worker_health_check.py
+++ trade_remedies_api/core/tests/test_service_worker_health_check.py
@@ -11,7 +11,6 @@ class HealthCheckTestCase(SimpleTestCase):
     @patch.object(settings, "REDIS_DATABASE_NUMBER", 0)
     @patch.object(settings, "OPENSEARCH_URI", "http://opensearch.com")
     def test_application_issue(self):
-
         # Test when the Redis service is up and running
         with patch("redis.StrictRedis.from_url") as mock_from_url:
             mock_redis = Mock()
@@ -25,7 +24,6 @@ class HealthCheckTestCase(SimpleTestCase):
     @patch.object(settings, "REDIS_DATABASE_NUMBER", 0)
     @patch.object(settings, "OPENSEARCH_URI", "http://opensearch.com")
     def test_application_issue_error(self):
-
         # Test when there is an issue in the redis service
         with patch("redis.StrictRedis.from_url") as mock_from_url:
             mock_redis = Mock()
