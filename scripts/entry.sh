e95616603bbc0d0ec0188d05845c5b03fd4c4f5d
Irene Grande <irene.grande@digital.trade.gov.uk>
2025-02-11 16:38:01 +0000

    Run collectstatic at build time

--- scripts/entry.sh
+++ scripts/entry.sh
@@ -1,11 +1,8 @@
 #!/bin/bash -e
 
-pwd 
-ls -al
 cd ./trade_remedies_api
 python ./manage.py migrate
 python ./manage.py load_sysparams
 python ./manage.py notify_env
-python ./manage.py collectstatic --noinput
 
 gunicorn config.wsgi --bind 0.0.0.0:$API_PORT --capture-output --config config/gunicorn.py
