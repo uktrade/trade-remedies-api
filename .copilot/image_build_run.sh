51c5ed97864ef7e80b1faba439064d984ee07af6
Irene Grande <irene.grande@digital.trade.gov.uk>
2025-02-11 16:59:30 +0000

    Populate environment variables required to build static assets

--- .copilot/image_build_run.sh
+++ .copilot/image_build_run.sh
@@ -2,4 +2,8 @@
 
 set -e
 
+export CASE_WORKER_ENVIRONMENT_KEY="example"
+export DJANGO_SECRET_KEY="example"
+export PUBLIC_ENVIRONMENT_KEY="example"
+
 python ./trade_remedies_api/manage.py collectstatic --noinput
