9493cbe67f53aa2773ea77e1dccd3159b59f1c7f
NickFitz <nick@nickfitz.co.uk>
2025-02-12 10:37:03 +0000

    Try `localhost` in `DATABASE_URL` value during build stage.

--- .copilot/image_build_run.sh
+++ .copilot/image_build_run.sh
@@ -5,6 +5,6 @@ set -e
 export CASE_WORKER_ENVIRONMENT_KEY="example"
 export DJANGO_SECRET_KEY="example"
 export PUBLIC_ENVIRONMENT_KEY="example"
-export DATABASE_URL=psql://postgres:postgres@banana:5432/trade_remedies
+export DATABASE_URL=psql://postgres:postgres@localhost:5432/trade_remedies
 
 python ./trade_remedies_api/manage.py collectstatic --noinput
