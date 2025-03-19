5a9f724ee9673d413145999b0fb3e7c5205046f7
santinomolinaro <santino.molinaro@mail>
2025-03-19 16:05:20 +0000

    New management command to clear a case Export Source

---
+++ trade_remedies_api/cases/management/commands/remove_export_source_from_case.py
@@ -0,0 +1,26 @@
+import logging
+
+from django.core.management.base import BaseCommand
+
+from cases.models import Case, ExportSource
+
+logger = logging.getLogger(__name__)
+
+
+class Command(BaseCommand):
+    help = """Remove the 'Export Source' field data from a case.
+    This will change the behaviour of a case relating to 'All Countries' 
+    rather than a specific one."""
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "case_name", 
+            type=str, 
+            help="Title of the case to update. Case Sensitive"
+        )
+
+    def handle(self, *args, **options):
+        logger.info("Clearing Export Source for given case: " + options["case_name"])
+        case_to_amend = Case.objects.get(name=options["case_name"])
+        ExportSource.objects.filter(case=case_to_amend[0]).delete()
+        logger.info("Export Source cleared for case: "+ options["case_name"])
