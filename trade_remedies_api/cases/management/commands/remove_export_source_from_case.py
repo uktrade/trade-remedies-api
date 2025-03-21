109f89475d8763af438a97edd62ce888de921c25
santinomolinaro <santino.molinaro@mail>
2025-03-21 12:29:46 +0000

    Final fix

--- trade_remedies_api/cases/management/commands/remove_export_source_from_case.py
+++ trade_remedies_api/cases/management/commands/remove_export_source_from_case.py
@@ -26,10 +26,10 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         logger.info("Clearing Export Source for given case: " + options["case_id"])
-        case_to_amend = Case.objects.get(name=options["case_name"])
+        case_to_amend = Case.objects.get(id=options["case_id"])
         if case_to_amend.type.id in ALL_COUNTRY_CASE_TYPES:
             ExportSource.objects.filter(case=case_to_amend).delete()
         else:
             raise Exception("Case identified is of a type not in the ALL_COUNTRY_CASE_TYPES list")
 
-        logger.info("Export Source cleared for case: " + options["case_name"])
+        logger.info("Export Source cleared for case: " + options["case_id"])
